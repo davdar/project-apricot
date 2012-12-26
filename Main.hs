b14afb04149cf21210c3e68946a33dc919271af6
David Darais <david.darais@gmail.com>
2012-12-26 16:37:55 -0700

    stuffs

--- Main.hs
+++ Main.hs
@@ -1,11 +1,15 @@
 module Main where
 
+import Control.Monad
+import Data.Array.IO
+import Data.Array.MArray
+import Data.IORef
+import Data.Map (Map)
+import System.IO.Unsafe
+import qualified Data.Map as Map
 import qualified Graphics.UI.SDL as SDL
 import qualified Graphics.UI.SDL.Image as SDL
-import qualified Data.Map as Map
-import Data.Map (Map)
-import Data.Array
-import Data.Array.MArray
+import Data.Functor
 
 data Player = Player
   { playerId :: Int
@@ -32,6 +36,7 @@ data WorldObject =
   | Closed
   | HeroObject Int
   | TownObject Int
+  deriving (Eq)
 
 data GameState = GameState 
   { gameStateWorld :: World
@@ -42,17 +47,18 @@ worldSize :: (Int,Int)
 worldSize = (100, 100)
 
 graphicsSize :: (Int,Int)
-graphicsSize = (800, 600)
+graphicsSize = (960, 640)
 
 tileSize :: (Int,Int)
-tileSize =
-  let (ww,wh) = worldSize
-      (gw,gh) = graphicsSize
-  in (gw / ww, gh / wh)
+tileSize = (32,32)
+
+currentScreenPos :: IORef (Int,Int)
+currentScreenPos = unsafePerformIO $ newIORef (0,0)
+{-# NOINLINE currentScreenPos #-}
 
 scenarioOne :: IO GameState
 scenarioOne = do
-  grid <- newArray worldSize Open
+  grid <- newArray ((0,0),worldSize) Open
   writeArray grid (5,5) Closed
   writeArray grid (5,6) Closed
   return $ GameState (World grid) Map.empty
@@ -64,13 +70,28 @@ main = do
   SDL.setVideoMode gw gh 32 []
   SDL.setCaption "Video Test!" "video test"
   mainSurf <- SDL.getVideoSurface
-  tileSurf <- SDL.load "froggy.png"
-  eventLoop
+  tileSurf <- SDL.load "froggy_32x32.png"
+  eventLoop mainSurf tileSurf
   SDL.quit
-  print "done"
   where
-    eventLoop = do
+    eventLoop mainSurf tileSurf = do
+      draw mainSurf tileSurf
       e <- SDL.waitEventBlocking
-      checkEvent e
-    checkEvent (SDL.KeyUp _) = return ()
-    checkEvent _ = eventLoop
+      checkEvent e mainSurf tileSurf
+    checkEvent (SDL.KeyUp (SDL.Keysym SDL.SDLK_RIGHT _ _)) mainSurf tileSurf = do
+      modifyIORef currentScreenPos $ \ (x,y) -> (x+1,y)
+      eventLoop mainSurf tileSurf
+    checkEvent (SDL.KeyUp (SDL.Keysym SDL.SDLK_LEFT _ _)) mainSurf tileSurf = do
+      modifyIORef currentScreenPos $ \ (x,y) -> (x,y+1)
+      eventLoop mainSurf tileSurf
+    checkEvent _ mainSurf tileSurf = eventLoop mainSurf tileSurf
+    draw mainSurf tileSurf = do
+      grid <- liftM (worldGrid . gameStateWorld) $ scenarioOne :: IO (IOArray (Int,Int) WorldObject)
+      assocs <- getAssocs grid :: IO [((Int,Int),WorldObject)]
+      (screenx,screeny) <- readIORef currentScreenPos
+      SDL.fillRect mainSurf Nothing (SDL.Pixel 0)
+      forM_ assocs $ \ ((x,y),e) ->
+        when (e == Closed) $ do
+          SDL.blitSurface tileSurf (Just (SDL.Rect 0 0 32 32)) mainSurf (Just (SDL.Rect (x*32 - screenx) (y*32 - screeny) 0 0))
+          return ()
+      SDL.flip mainSurf
---
+++ MainWrapper.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+module MainWrapper where
+
+import Main (main)
+
+foreign export ccall "haskell_main" main :: IO ()
