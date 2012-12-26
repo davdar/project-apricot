2db0c7778faaa3079d5c75652a2cb632957e08ef
David Darais <david.darais@gmail.com>
2012-12-26 15:55:17 -0700

    froggy

--- Main.hs
+++ Main.hs
@@ -1,31 +1,76 @@
 module Main where
 
-import Control.Monad
-import Data.IORef
-import Graphics.UI.GLUT
+import qualified Graphics.UI.SDL as SDL
+import qualified Graphics.UI.SDL.Image as SDL
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.Array
+import Data.Array.MArray
+
+data Player = Player
+  { playerId :: Int
+  , playerHeroes :: [Int]
+  , playerTowns :: [Int]
+  }
+
+data Hero = Hero
+  { heroId :: Int
+  , heroLoc :: (Int, Int)
+  }
+
+data Town = Town
+  { townId :: Int
+  , townLoc :: (Int,Int)
+  }
+
+data World = World
+  { worldGrid :: IOArray (Int,Int) WorldObject
+  }
+
+data WorldObject =
+    Open
+  | Closed
+  | HeroObject Int
+  | TownObject Int
+
+data GameState = GameState 
+  { gameStateWorld :: World
+  , gameStatePlayers :: Map Int Player
+  }
+
+worldSize :: (Int,Int)
+worldSize = (100, 100)
+
+graphicsSize :: (Int,Int)
+graphicsSize = (800, 600)
+
+tileSize :: (Int,Int)
+tileSize =
+  let (ww,wh) = worldSize
+      (gw,gh) = graphicsSize
+  in (gw / ww, gh / wh)
+
+scenarioOne :: IO GameState
+scenarioOne = do
+  grid <- newArray worldSize Open
+  writeArray grid (5,5) Closed
+  writeArray grid (5,6) Closed
+  return $ GameState (World grid) Map.empty
 
 main :: IO ()
-main = runGUI
-
-runGUI :: IO ()
-runGUI = do
-  (progName, _) <- getArgsAndInitialize
-  createWindow progName
-  displayCallback $= display
-  reshapeCallback $= Just reshape
-  keyboardMouseCallback $= Just keyboardMouse
-  mainLoop
-
-display :: IO ()
-display = do
-  clear [ ColorBuffer ]
-  flush
-
-reshape :: Size -> IO ()
-reshape size = do
-  viewport $= (Position 0 0, size)
-  postRedisplay Nothing
-
-keyboardMouse :: Key -> KeyState -> Modifiers -> Position -> IO () 
-keyboardMouse key keyState modifiers position = do
-  return ()
+main = do
+  let (gw,gh) = graphicsSize 
+  SDL.init [SDL.InitEverything]
+  SDL.setVideoMode gw gh 32 []
+  SDL.setCaption "Video Test!" "video test"
+  mainSurf <- SDL.getVideoSurface
+  tileSurf <- SDL.load "froggy.png"
+  eventLoop
+  SDL.quit
+  print "done"
+  where
+    eventLoop = do
+      e <- SDL.waitEventBlocking
+      checkEvent e
+    checkEvent (SDL.KeyUp _) = return ()
+    checkEvent _ = eventLoop
