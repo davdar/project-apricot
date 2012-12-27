f03ba8bd8c5399c0120e0ec66f91d40114920759
David Darais <david.darais@gmail.com>
2012-12-26 17:07:52 -0700

    cleaned up

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Monad
+import Control.Monad
 import Data.Array.IO
 import Data.Array.MArray
 import Data.IORef
@@ -10,38 +11,13 @@ import qualified Data.Map as Map
 import qualified Graphics.UI.SDL as SDL
 import qualified Graphics.UI.SDL.Image as SDL
 import Data.Functor
+import Assets
+import GameState
+import System.Exit
 
-data Player = Player
-  { playerId :: Int
-  , playerHeroes :: [Int]
-  , playerTowns :: [Int]
-  }
-
-data Hero = Hero
-  { heroId :: Int
-  , heroLoc :: (Int, Int)
-  }
-
-data Town = Town
-  { townId :: Int
-  , townLoc :: (Int,Int)
-  }
-
-data World = World
-  { worldGrid :: IOArray (Int,Int) WorldObject
-  }
-
-data WorldObject =
-    Open
-  | Closed
-  | HeroObject Int
-  | TownObject Int
-  deriving (Eq)
-
-data GameState = GameState 
-  { gameStateWorld :: World
-  , gameStatePlayers :: Map Int Player
-  }
+currentScreenPos :: IORef (Int,Int)
+currentScreenPos = unsafePerformIO $ newIORef (0,0)
+{-# NOINLINE currentScreenPos #-}
 
 worldSize :: (Int,Int)
 worldSize = (100, 100)
@@ -52,10 +28,6 @@ graphicsSize = (960, 640)
 tileSize :: (Int,Int)
 tileSize = (32,32)
 
-currentScreenPos :: IORef (Int,Int)
-currentScreenPos = unsafePerformIO $ newIORef (0,0)
-{-# NOINLINE currentScreenPos #-}
-
 scenarioOne :: IO GameState
 scenarioOne = do
   grid <- newArray ((0,0),worldSize) Open
@@ -63,35 +35,51 @@ scenarioOne = do
   writeArray grid (5,6) Closed
   return $ GameState (World grid) Map.empty
 
+mainDraw :: (SDL.Surface -> IO ()) -> IO ()
+mainDraw action = do
+  s <- SDL.getVideoSurface
+  SDL.fillRect s Nothing (SDL.Pixel 0)
+  action s
+  SDL.flip s
+  
 main :: IO ()
 main = do
-  let (gw,gh) = graphicsSize 
   SDL.init [SDL.InitEverything]
+  let (gw,gh) = graphicsSize 
   SDL.setVideoMode gw gh 32 []
-  SDL.setCaption "Video Test!" "video test"
-  mainSurf <- SDL.getVideoSurface
-  tileSurf <- SDL.load "froggy_32x32.png"
-  eventLoop mainSurf tileSurf
+  SDL.setCaption "Project Apricot" "apricot"
+  loadAssets
+  writeIORef gameState =<< scenarioOne
+  eventLoop
+
+exit :: IO ()
+exit = do
   SDL.quit
-  where
-    eventLoop mainSurf tileSurf = do
-      draw mainSurf tileSurf
-      e <- SDL.waitEventBlocking
-      checkEvent e mainSurf tileSurf
-    checkEvent (SDL.KeyUp (SDL.Keysym SDL.SDLK_RIGHT _ _)) mainSurf tileSurf = do
-      modifyIORef currentScreenPos $ \ (x,y) -> (x+1,y)
-      eventLoop mainSurf tileSurf
-    checkEvent (SDL.KeyUp (SDL.Keysym SDL.SDLK_LEFT _ _)) mainSurf tileSurf = do
-      modifyIORef currentScreenPos $ \ (x,y) -> (x,y+1)
-      eventLoop mainSurf tileSurf
-    checkEvent _ mainSurf tileSurf = eventLoop mainSurf tileSurf
-    draw mainSurf tileSurf = do
-      grid <- liftM (worldGrid . gameStateWorld) $ scenarioOne :: IO (IOArray (Int,Int) WorldObject)
-      assocs <- getAssocs grid :: IO [((Int,Int),WorldObject)]
-      (screenx,screeny) <- readIORef currentScreenPos
-      SDL.fillRect mainSurf Nothing (SDL.Pixel 0)
-      forM_ assocs $ \ ((x,y),e) ->
-        when (e == Closed) $ do
-          SDL.blitSurface tileSurf (Just (SDL.Rect 0 0 32 32)) mainSurf (Just (SDL.Rect (x*32 - screenx) (y*32 - screeny) 0 0))
-          return ()
-      SDL.flip mainSurf
+  exitSuccess
+  
+eventLoop :: IO ()
+eventLoop = do
+  mainDraw $ \ s -> do
+    grid <- getAssocs <=< liftM (worldGrid . gameStateWorld) $ readIORef gameState
+    (screenx,screeny) <- readIORef currentScreenPos
+    tile <- liftM assetsFroggy $ readIORef assets
+    forM_ grid $ \ ((x,y),e) ->
+      when (e == Closed) $ do
+        SDL.blitSurface tile (Just (SDL.Rect 0 0 32 32)) 
+                        s (Just (SDL.Rect (x*32 - screenx) (y*32 - screeny) 0 0))
+        return ()
+  checkEvent =<< SDL.waitEventBlocking
+
+checkEvent :: SDL.Event -> IO ()
+checkEvent e = do
+  case e of
+    SDL.KeyUp (SDL.Keysym k mod uni) ->
+      case k of
+        SDL.SDLK_LEFT -> modifyIORef currentScreenPos $ \ (x,y) -> (x-1,y)
+        SDL.SDLK_RIGHT -> modifyIORef currentScreenPos $ \ (x,y) -> (x+1,y)
+        SDL.SDLK_UP -> modifyIORef currentScreenPos $ \ (x,y) -> (x,y-1)
+        SDL.SDLK_DOWN -> modifyIORef currentScreenPos $ \ (x,y) -> (x,y+1)
+        SDL.SDLK_q -> exit
+        _ -> return ()
+    _ -> return ()
+  eventLoop
